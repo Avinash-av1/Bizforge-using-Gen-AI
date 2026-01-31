5c445fa971db51ea5caaff17690027800fe12b4f
Avinash-av1 <uavinash2019@gmail.com>
2026-01-31 17:40:45 +0530

    https://bizforge-using-gen-ai.onrender.com

--- frontend/Script.js
+++ frontend/Script.js
@@ -1,4 +1,4 @@
-const API_BASE = "http://127.0.0.1:8000/api";
+const API_BASE = "https://bizforge-using-gen-ai.onrender.com";
 const BACKEND_URL = "http://127.0.0.1:8000";
 
 // ---------------- COMMON OUTPUT HANDLER ----------------
